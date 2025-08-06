74108e6e553408828e48fd855d6e4085f75b2785
SwapnaSarita01 <87479827+SwapnaSarita01@users.noreply.github.com>
2025-08-06 10:47:14 +0530

    backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
+    bucket         = "devsecops_project"
+    region         = "ap-south-1"
+    key            = "Devsecops-3-Tier-Kubernetes-Project/Jenkins-Server-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
   }
